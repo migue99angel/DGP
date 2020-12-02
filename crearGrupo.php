bb163edd9fb61f20b7de724c9e6b643bf49270f9
sergiostars27 <sergiostars27@gmail.com>
2020-12-02 18:57:12 +0100

    Se ha comenzado a delinear la creacion de grupo

--- crearGrupo.php
+++ crearGrupo.php
@@ -7,6 +7,20 @@
 
   $variablesParaTwig = [];
 
+  session_start();
+
+  if(isset($SESSION['facilitador'])) {
+    $conexion = new ConexionBD();
+    $personas = $conexion->getAllPersonas();
+  }
+
+  if($_SERVER['REQUEST_METHOD'] === "POST"){
+    $idGrupo = $_POST['id'];
+    $nombreGrupo = $_POST['nombre'];
+    $participantesGrupo = $_POST['participantes']
+    $conexion->crearGrupo($idGrupo,$nombreGrupo,$participantesGrupo);
+  }
+
   echo $twig->render('crearGrupo.html', $variablesParaTwig);
 
 ?>
