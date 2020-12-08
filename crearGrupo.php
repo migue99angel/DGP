bdf069a24c46e36581ce613e6b49ffd7686217cd
sergiostars27 <sergiostars27@gmail.com>
2020-12-08 18:32:14 +0100

    Gestión de grupos en proceso

--- crearGrupo.php
+++ crearGrupo.php
@@ -1,24 +1,35 @@
 <?php
 
   require_once "/usr/local/lib/php/vendor/autoload.php";
+  require_once "./modelo/conexionBD.php";
+  require_once "./modelo/facilitador.php";
 
   $loader = new \Twig\Loader\FilesystemLoader('templates');
   $twig = new \Twig\Environment($loader);
 
   $variablesParaTwig = [];
+  $variablesParaTwig['botonAtras'] = True;
 
   session_start();
+  $conexion = new ConexionBD();
 
-  if(isset($SESSION['facilitador'])) {
-    $conexion = new ConexionBD();
-    $personas = $conexion->getAllPersonas();
+
+  if(!isset($_POST['nombre'])){
+    $variablesParaTwig['paginaAnterior'] = 'principalFacilitador.php';
+    $variablesParaTwig['seleccionMiembros'] = False;
+
   }
-
-  if($_SERVER['REQUEST_METHOD'] === "POST"){
-    $idGrupo = $_POST['id'];
-    $nombreGrupo = $_POST['nombre'];
-    $participantesGrupo = $_POST['participantes'];
-    $conexion->crearGrupo($idGrupo,$nombreGrupo,$participantesGrupo);
+  else{
+    //$_SESSION['facilitador'].crearGrupo($_POST['nombre']);
+    $variablesParaTwig['paginaAnterior'] = 'crearGrupo.php';
+    $variablesParaTwig['accionCheckbox'] = 'crearGrupo.php';
+    $variablesParaTwig['idCheckbox'] = 'listaPersonas';
+    $variablesParaTwig['encCheckbox'] = 'multipart/form-data';
+    $variablesParaTwig['tituloLista'] = 'Lista de Personas';
+    $variablesParaTwig['tipoLista'] = 'personas';
+    //$variablesParaTwig['elementos'] = $conexion->getAllPersonas();
+    $variablesParaTwig['valorSubmitCheckbox'] = 'Insertar en el Grupo';
+    $variablesParaTwig['seleccionMiembros'] = True;
   }
 
   echo $twig->render('crearGrupo.html', $variablesParaTwig);
