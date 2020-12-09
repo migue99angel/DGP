672668c266500f831b71e7d3bf9023b15eb10a52
José Mª Gómez García <33324337+gomezjosemaria@users.noreply.github.com>
2020-12-09 23:11:56 +0100

    Añadido vista desasignación de ejercicios

--- desasignarEjercicio.php
+++ desasignarEjercicio.php
@@ -1,11 +1,36 @@
 <?php
 
   require_once "/usr/local/lib/php/vendor/autoload.php";
+  require_once "./modelo/conexionBD.php";
+  require_once "./modelo/asigna.php";
+
   $loader = new \Twig\Loader\FilesystemLoader('templates');
   $twig = new \Twig\Environment($loader);
-
+  
+  session_start();
   $variablesParaTwig = [];
+  
+  if (!isset($_SESSION['facilitador'])) {
+    exit();
+  }
+  
+  $conexion = new ConexionBD();
+  $res = $conexion->getAllEjerciciosAsignados();
+
+  $variablesParaTwig['lista'] = array(
+    'accionCheckbox' => 'desasignarEjercicio.php',
+    'idCheckbox'     => 'listaEjerciciosAsignados',
+    'encCheckbox'    => 'multipart/form-data',
+    'tituloLista'    => 'Lista de Ejercicios Asignados',
+    'elementos'      => $res,
+    //'numeroOcultos'  => 0,
+    //'inputOcultos'   => array('listaPersonas'),
+    'valorSubmitCheckbox' => 'Desasignar ejercicios'
+  );
+  
+  $variablesParaTwig['botonAtras'] = true;
+  $variablesParaTwig['paginaAnterior'] = 'principalFacilitador.php';
 
   echo $twig->render('desasignarEjercicio.html', $variablesParaTwig);
 
-  ?>
+ ?>
--- modelo/asigna.php
+++ modelo/asigna.php
@@ -5,17 +5,17 @@
 		private $idFacilitador;
 		private $idPersona;
 		private $fechaAsignacion;
-		private $tituloEjercicio;
+		private $titulo;
 		private $nombreFacilitador;
 		private $nombrePersona;
 		
-		public function __construct($idEjercicio=-1, $idFacilitador=-1, $idPersona=-1, $fechaAsignacion="", $tituloEjercicio="", $nombreFacilitador="", $nombrePersona="")
+		public function __construct($idEjercicio=-1, $idFacilitador=-1, $idPersona=-1, $fechaAsignacion="", $titulo="", $nombreFacilitador="", $nombrePersona="")
 		{
 			$this->idEjercicio = $idEjercicio;
 			$this->idFacilitador = $idFacilitador;
 			$this->idPersona = $idPersona;
 			$this->fechaAsignacion = $fechaAsignacion;
-			$this->tituloEjercicio = $tituloEjercicio;
+			$this->titulo = $titulo;
 			$this->nombreFacilitador = $nombreFacilitador;
 			$this->nombrePersona = $nombrePersona;
 		}
@@ -25,9 +25,19 @@
 			return $this->idEjercicio;
 		}
 		
+		public function setIdEjercicio($idEjercicio)
+		{
+			$this->idEjercicio = $idEjercicio;
+		}
+			
 		public function getIdFacilitador()
 		{
 			return $this->idFacilitador;
+		}
+		
+		public function setIdFacilitador($idFacilitador)
+		{
+			$this->idFacilitador = $idFacilitador;
 		}
 		
 		public function getIdPersona()
@@ -35,14 +45,29 @@
 			return $this->idPersona;
 		}
 		
+		public function setIdPersona($idPersona)
+		{
+			$this->idPersona = $idPersona;
+		}
+		
 		public function getFechaAsignacion()
 		{
 			return $this->fechaAsignacion;
 		}
 		
-		public function getTituloEjercicio()
+		public function setFechaAsignacion($fechaAsignacion)
 		{
-			return $this->tituloEjercicio;
+			$this->fechaAsignacion = $fechaAsignacion;
+		}
+		
+		public function getTitulo()
+		{
+			return $this->titulo;
+		}
+		
+		public function setTitulo($titulo)
+		{
+			$this->titulo = $titulo;
 		}
 		
 		public function getNombreFacilitador()
@@ -50,9 +75,19 @@
 			return $this->nombreFacilitador;
 		}
 		
+		public function setNombreFacilitador($nombreFacilitador)
+		{
+			$this->nombreFacilitador = $nombreFacilitador;
+		}
+		
 		public function getNombrePersona()
 		{
 			return $this->nombrePersona;
+		}
+		
+		public function setNombrePersona($nombrePersona)
+		{
+			$this->nombrePersona = $nombrePersona;
 		}
 	}
 ?>
