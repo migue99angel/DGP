2ed7b1b724bbafb574dbb1f0359e90d6e680bd67
José Mª Gómez García <33324337+gomezjosemaria@users.noreply.github.com>
2020-12-16 19:49:59 +0100

    Completando el modelo de Asigna

--- modelo/asigna.php
+++ modelo/asigna.php
@@ -8,8 +8,11 @@
 		private $titulo;
 		private $nombreFacilitador;
 		private $nombrePersona;
+		private $fechaResolucion;
+		private $valoracionPersona;
+		private $archivoAdjuntoSolucion;
 		
-		public function __construct($idEjercicio=-1, $idFacilitador=-1, $idPersona=-1, $fechaAsignacion="", $titulo="", $nombreFacilitador="", $nombrePersona="")
+		public function __construct($idEjercicio=-1, $idFacilitador=-1, $idPersona=-1, $fechaAsignacion="", $titulo="", $nombreFacilitador="", $nombrePersona="", $fechaResolucion="", $valoracionPersona="", $archivoAdjuntoSolucion="")
 		{
 			$this->idEjercicio = $idEjercicio;
 			$this->idFacilitador = $idFacilitador;
@@ -18,6 +21,9 @@
 			$this->titulo = $titulo;
 			$this->nombreFacilitador = $nombreFacilitador;
 			$this->nombrePersona = $nombrePersona;
+			$this->fechaResolucion = $fechaResolucion;
+			$this->valoracionPersona = $valoracionPersona;
+			$this->archivoAdjuntoSolucion = $archivoAdjuntoSolucion;
 		}
 		
 		public function getIdEjercicio()
@@ -88,6 +94,36 @@
 		public function setNombrePersona($nombrePersona)
 		{
 			$this->nombrePersona = $nombrePersona;
+		}
+		
+		public function getFechaResolucion()
+		{
+			return $this->fechaResolucion;
+		}
+		
+		public function setFechaResolucion($fechaResolucion)
+		{
+			$this->fechaResolucion = $fechaResolucion;
+		}
+		
+		public function getValoracionPersona()
+		{
+			return $this->valoracionPersona;
+		}
+		
+		public function setValoracionPersona($valoracionPersona)
+		{
+			$this->valoracionPersona = $valoracionPersona;
+		}
+		
+		public function getArchivoAdjuntoSolucion()
+		{
+			return $this->archivoAdjuntoSolucion;
+		}
+		
+		public function setArchivoAdjuntoSolucion($archivoAdjuntoSolucion)
+		{
+			$this->archivoAdjuntoSolucion = $archivoAdjuntoSolucion;
 		}
 	}
 ?>
