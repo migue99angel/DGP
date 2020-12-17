0aa68dbaf21854e330eb51f05fa689fc60589773
frandominguez03 <d3vcho@gmail.com>
2020-12-17 23:44:16 +0100

    Cambios sustanciales en las plantillas. Modificados también archivos Javascript. Consistencia en general

--- templates/js/mostrarEjercicio.js
+++ templates/js/mostrarEjercicio.js
@@ -1,10 +1,3 @@
-var nota = document.getElementById("nota").innerHTML;
-for(var i=0; i < nota; i++)
-{
-    document.getElementById("estrella"+i).style.color = "orange" ;
-}
-
-
 var botonChat = document.getElementById('botonChat');
 
 if (botonChat) {
@@ -13,3 +6,11 @@ if (botonChat) {
     });
 }
 
+function updateTextInput(val) {
+    document.getElementById('textInput').innerHTML = val;
+}
+
+function updateFile(val) {
+    var valor = val.replace('C:\\fakepath\\', '');
+    document.getElementById('archivoSeleccionado').innerHTML = valor;
+}
