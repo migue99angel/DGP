c742d5430feef0f95df1e3760a24a83802ab1bff
Dunspa <jlgallego99@correo.ugr.es>
2020-12-18 14:09:15 +0100

    Avanzado bastante cambio de divs por botones

---
+++ templates/js/principalFacilitador.js
@@ -0,0 +1,47 @@
+var botonPrincipalFacilitador = document.getElementById("botonFaci1");
+
+if (botonPrincipalFacilitador) {
+    botonPrincipalFacilitador.addEventListener('click', (event) => {
+        document.location.href = "asignarEjercicio.php";
+    });
+}
+
+botonPrincipalFacilitador = document.getElementById("botonFaci2");
+
+if (botonPrincipalFacilitador) {
+    botonPrincipalFacilitador.addEventListener('click', (event) => {
+        document.location.href = "listaEjercicios.php";
+    });
+}
+
+botonPrincipalFacilitador = document.getElementById("botonFaci3");
+
+if (botonPrincipalFacilitador) {
+    botonPrincipalFacilitador.addEventListener('click', (event) => {
+        document.location.href = "crearEjercicio.php";
+    });
+}
+
+botonPrincipalFacilitador = document.getElementById("botonFaci4");
+
+if (botonPrincipalFacilitador) {
+    botonPrincipalFacilitador.addEventListener('click', (event) => {
+        document.location.href = "administracionGrupo.php";
+    });
+}
+
+botonPrincipalFacilitador = document.getElementById("botonFaci5");
+
+if (botonPrincipalFacilitador) {
+    botonPrincipalFacilitador.addEventListener('click', (event) => {
+        document.location.href = "chat_PlaceHolder.php";
+    });
+}
+
+botonPrincipalFacilitador = document.getElementById("botonFaci6");
+
+if (botonPrincipalFacilitador) {
+    botonPrincipalFacilitador.addEventListener('click', (event) => {
+        document.location.href = "desasignarEjercicio.php.php";
+    });
+}
